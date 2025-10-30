f7a4581383453420055c9952d025a1d277667207
Rihad Jahan Opu <rihadjahanopu@gmail.com>
2025-10-30 14:07:17 +0600

    time

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Rihad Jahan Opu");
+
+const button = document.createElement("button");
+button.innerText = "Click Me";
+document.body.appendChild(button);
+
+button.addEventListener("click", () => {
+	alert("Welcome to my website");
+});
